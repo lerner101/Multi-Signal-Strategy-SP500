b9d33b66e451fc21ea5136d4b6b147a42e3f55fb
Nathaniel Lee <leenathaniel001@gmail.com>
2025-09-30 16:22:15 -0500

    Vol Breakout Strat

--- VolatilityBreakoutStrat.py
+++ VolatilityBreakoutStrat.py
@@ -1,37 +1,24 @@
-'''class MovingAverageStrategy(Strategy):
-    """
-    Signals (executed next day by the base runner):
-      +1  -> BUY one share when MA20 crosses ABOVE MA50
-      -1  -> SELL one share when MA20 crosses BELOW MA50
-       0  -> HOLD
-    """
-
-    def compute_signals(self, prices: pd.DataFrame) -> pd.DataFrame:
-        # Rolling means; min_periods ensures we don't trade before both MAs exist
-        ma20 = prices.rolling(window=20, min_periods=20).mean()
-        ma50 = prices.rolling(window=50, min_periods=50).mean()
-
-        # Crossovers
-        above_yday = ma20.shift(1) > ma50.shift(1)
-        above_today = ma20 > ma50
-
-        cross_up   = (~above_yday) & (above_today)   # 0/1 boolean
-        cross_down = (above_yday) & (~above_today)   # 0/1 boolean
-
-        signals = cross_up.astype(int) - cross_down.astype(int)  # in {-1,0,1}
-
-        # Don’t trade where MAs aren’t valid yet
-        valid = ma50.notna() & ma20.notna()
-        signals = signals.where(valid, 0).astype(int)
-
-        return signals
-'''
 from Strategy import Strategy
+import pandas as pd
 
 class Volatility_Breakout_Strategy(Strategy):
-    """
-    Signals (executed next day by the base runner):
-      +1  -> BUY one share when MA20 crosses ABOVE MA50
-      -1  -> SELL one share when MA20 crosses BELOW MA50
-       0  -> HOLD
-    """
+  """
+  Signals (executed next day by the base runner):
+    +1  -> BUY one share when daily return > roll 20d STD
+    -1  -> SELL one share when daily return < roll 20d STD
+    0  -> No action
+  """
+  def __init__(self,name = 'Volatility_Breakout'):
+    super().__init__(name = name)
+    self.tickers = None # list[str]
+    self.T = None       # time length
+    self.N = None       # no of tickers
+
+  def compute_signals(self, prices) -> pd.DataFrame:
+    std20 = prices.rolling(window = 20, min_periods=20).std()
+    rets = prices.pct_change()
+    signals = pd.DataFrame(0,index = prices.index, columns= prices.columns)
+    signals[rets > std20] = 1
+    signals[rets < -std20] = -1
+
+    return signals
